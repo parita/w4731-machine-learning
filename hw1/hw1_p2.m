7db921313162414e025344652da05195febffa01
Parita Pooj <pari.pooj@gmail.com>
2016-09-22 00:29:49 -0400

    Completed Problem 2 with Latex file and README

--- hw1/hw1_p2.m
+++ hw1/hw1_p2.m
@@ -4,17 +4,13 @@ function hw1_p2(data, labels, testdata, testlabels, numsamples_list)
         error_stddev = zeros(size(error_list));
         for i = 1:size(numsamples_list, 2)
                 n = numsamples_list(1, i)
-                numruns = 1;
-                error_run = zeros(1, numruns);
-                for run = 1:numruns
-                        [ndata, nlabels] = sel_prototype(data, labels, n);
-                        preds = classify_1nn(ndata, nlabels, testdata, ...
-                                                             testlabels);
-                        error_run(1, run) = test_error_rate(preds, testlabels);
-                end
-                error_list(1, i) = sum(error_run)/numruns;
-                error_stddev(1, i) = std(error_run);
+                [ndata, nlabels] = sel_prototype(data, labels, n);
+                preds = classify_1nn(ndata, nlabels, testdata, ...
+                                                     testlabels);
+                error_list(1, i) = test_error_rate(preds, testlabels)
+                clear ndata nlabels preds
         end
+
         save('p2_error_rates', 'error_list');
         figure;
         plot(numsamples_list, error_list);
@@ -24,7 +20,16 @@ function hw1_p2(data, labels, testdata, testlabels, numsamples_list)
 end
 
 function [ndata, nlabels] = sel_prototype(data, labels, n)
-        [idx, ndata] = kmeans(data, n);
-        data_idx = [1:size(data, 1)];
-        nlabels = accumarray(idx, data_idx, [], @(x) mode(labels(x, :)));
+        ndata = [];
+        nlabels = [];
+        for i = 0:9
+                [r, c] = find(labels == i);
+                d = data(r, :);
+                l = labels(r, :);
+                [idx, subset_ndata] =  fkmeans(d, n/10);
+                data_idx = [1:size(d, 1)];
+                subset_nlabels = accumarray(idx, data_idx, [], @(x) mode(l(x, :)));
+                ndata = [ndata; subset_ndata];
+                nlabels = [nlabels; subset_nlabels];
+        end
 end
--- hw1/run_hw1.m
+++ hw1/run_hw1.m
@@ -3,5 +3,5 @@ load('ocr.mat')
 N = [1000, 2000, 4000, 8000];
 disp('Running Problem 1 code ...')
 hw1_p1(data, labels, testdata, testlabels, N);
-disk('Running Problem 2 code ...')
-hw2_p2(data, labels, testdata, testlabels, N);
+disp('Running Problem 2 code ...')
+hw1_p2(data, labels, testdata, testlabels, N);
