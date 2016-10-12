bbe5ff175654b4212f91a16aee2e6a80ff882ed5
Parita Pooj <pari.pooj@gmail.com>
2016-10-12 06:27:28 -0400

    Added Average Online Perceptron for training

--- hw3/hw3_preprocess.py
+++ hw3/hw3_preprocess.py
@@ -1,4 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from scipy.sparse import csr_matrix
+import pickle
 import numpy as np
 import math
 
@@ -29,7 +31,7 @@ def bigram_tf(data):
     vectorizer = CountVectorizer(tokenizer = tokenize, ngram_range = (2, 2))
     tf = vectorizer.fit_transform(data)
     tokens = vectorizer.get_feature_names()
-    return tf.toarray(), tokens
+    return tf, tokens
 
 def cross_validate(data, labels, kfolds, n):
     sample = n/kfolds;
@@ -47,20 +49,50 @@ def cross_validate(data, labels, kfolds, n):
         test_labels = np.append(data[trend:n], data[tebegin:teend])
         tf, tokens = unigram_tf(training_data)
         # tf = np.concatenate((tf, np.ones([np.shape(tf)[0], 1])), axis = 1)
-        np.save('tf_uni_' + str(ifold), tf)
+        with open('tf_uni_' + str(ifold) + '.pkl', 'w') as f:
+            pickle.dump(tf, f)
+        trsize = n * (kfolds - 1) / kfolds
+        print len(tokens)
+        W = online_perceptron_train(tf, labels, trsize, len(tokens))
+        np.save('Classifier', W)
+
+def online_perceptron_train(features, y, n, nfeatures):
+    W = np.zeros([1, nfeatures + 1])
+    wt = W[0, :]
+    features = csr_matrix(features)
+    for i in range(n):
+        if (i % 5000 == 0):
+            print "Processing", i
+        x = features.getrow(i).toarray()
+        x = np.append(x, 1)
+        if (np.dot(wt, x)*y[i] <= 0):
+            c = y[i]
+            if (y[i] == 0): 
+                c = -1
+            wt = wt + c*x
+        W = W + wt
+    W = W / (2*n + 1)
+    return W
+
+def online_perceptron_test(tokens, testfeatures, testlabels, n):
+    pass
 
 if __name__ == "__main__":
     filename = 'reviews_tr.csv'
     #data, labels = get_data_labels(filename)
     #data = data[1:200000]
     #labels = labels[1:200000]
-    data = load('data.npy')
-    labels = load('labels.npy')
-    tf, tokens = unigram_tf(data)
-    np.save('tf', tf.toarray())
-    np.save('features', tokens)
-    log_idf = unigram_tfidf(tf)
-    np.save('log_idf', log_idf)
-    bitf, bitokens = bigram_tf(data[1:200000])
-    np.save('bitf', bitf)
-    np.save('bigram_features', bitokens)
+    print "Loading data ..."
+    data = np.load('data.npy')
+    labels = np.load('labels.npy')
+    # tf, tokens = unigram_tf(data[1:100])
+    print "Cross-validating ..."
+    cross_validate(data, labels, 5, 200000)
+
+    #np.save('tf', tf.toarray())
+    #np.save('features', tokens)
+    #log_idf = unigram_tfidf(tf)
+    #np.save('log_idf', log_idf)
+    #bitf, bitokens = bigram_tf(data[1:200000])
+    #np.save('bitf', bitf.toarray())
+    #np.save('bigram_features', bitokens)
