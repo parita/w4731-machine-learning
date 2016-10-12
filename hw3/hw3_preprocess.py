eb3ececf8e4e8a1010c8c0208a31f61b6ee83b36
Parita Pooj <pari.pooj@gmail.com>
2016-10-12 15:36:01 -0400

    hw3: Completed Averaged Perceptron modification 1 with unigram

--- hw3/hw3_preprocess.py
+++ hw3/hw3_preprocess.py
@@ -14,12 +14,16 @@ def get_data_labels(filename):
 def tokenize(s):
     return s.split()
 
-def unigram_tf(data):
-    vectorizer = CountVectorizer(tokenizer = tokenize)
+def unigram_tf(data, vectorizer):
     tf = vectorizer.fit_transform(data)
     tokens = vectorizer.get_feature_names()
     return tf, tokens
 
+def unigram_tf_transform(data, vectorizer):
+    tf = vectorizer.transform(data)
+    return tf
+
+"""
 def unigram_tfidf(tf):
     tfidf_transformer = TfidfTransformer(smooth_idf = False)
     X = tfidf_transformer.fit_transform(tf);
@@ -32,33 +36,43 @@ def bigram_tf(data):
     tf = vectorizer.fit_transform(data)
     tokens = vectorizer.get_feature_names()
     return tf, tokens
+"""
 
 def cross_validate(data, labels, kfolds, n):
+    # Size of Test Data
     sample = n/kfolds;
+    # Size of Training Data
+    trsize = n * (kfolds - 1) / kfolds
+    error_rate = 0
     for ifold in range(kfolds):
         print "Folding ", ifold
+        vectorizer = CountVectorizer(tokenizer = tokenize)
+        # Indexes to combine training and test data for kfold
         ileft = (ifold+1)*sample - sample;
         trbegin = ifold*sample
         trend = min(n, (ifold + kfolds - 1)*sample)
         trend2 = max(0, (ifold - 1)*sample)
         tebegin = max(0, (ifold - 1)*sample)
         teend = ifold*sample
+        # Define Training and Test Data
         training_data = np.append(data[trbegin:trend], data[0:trend2])
-        training_labels = np.append(data[trbegin:trend], data[0:trend2])
+        training_labels = np.append(labels[trbegin:trend], labels[0:trend2])
         test_data = np.append(data[trend:n], data[tebegin:teend])
-        test_labels = np.append(data[trend:n], data[tebegin:teend])
-        tf, tokens = unigram_tf(training_data)
-        # tf = np.concatenate((tf, np.ones([np.shape(tf)[0], 1])), axis = 1)
-        with open('tf_uni_' + str(ifold) + '.pkl', 'w') as f:
-            pickle.dump(tf, f)
-        trsize = n * (kfolds - 1) / kfolds
-        print len(tokens)
+        test_labels = np.append(labels[trend:n], labels[tebegin:teend])
+        # Compute Features
+        tf, tokens = unigram_tf(training_data, vectorizer)
+        tf_test = unigram_tf_transform(test_data, vectorizer)
+        # Train for Averaged Online Perceptron
         W = online_perceptron_train(tf, labels, trsize, len(tokens))
-        np.save('Classifier', W)
+        # Get error rate on test data
+        err = online_perceptron_test(tf_test, test_labels, W, sample) 
+        error_rate = error_rate + err
+    error_rate = error_rate / kfolds
+    print "Error:", error_rate, "%"
 
 def online_perceptron_train(features, y, n, nfeatures):
-    W = np.zeros([1, nfeatures + 1])
-    wt = W[0, :]
+    W = np.zeros([nfeatures + 1,])
+    wt = W
     features = csr_matrix(features)
     for i in range(n):
         if (i % 5000 == 0):
@@ -74,8 +88,20 @@ def online_perceptron_train(features, y, n, nfeatures):
     W = W / (2*n + 1)
     return W
 
-def online_perceptron_test(tokens, testfeatures, testlabels, n):
-    pass
+def online_perceptron_test(testfeatures, testlabels, W, n):
+    err = 0
+    testfeatures = csr_matrix(testfeatures)
+    for i in range(n):
+        if (i % 5000 == 0):
+            print "Processing", i
+        x = testfeatures.getrow(i).toarray()
+        x = np.append(x, 1)
+        y = testlabels[i]
+        if (y == 0):
+            y = -1
+        if (np.dot(W, x)*y <= 0):
+            err = err + 1
+    return (err * 100 / n)
 
 if __name__ == "__main__":
     filename = 'reviews_tr.csv'
@@ -87,6 +113,7 @@ if __name__ == "__main__":
     labels = np.load('labels.npy')
     # tf, tokens = unigram_tf(data[1:100])
     print "Cross-validating ..."
+    #cross_validate(data[0:10], labels[0:10], 5, 10)
     cross_validate(data, labels, 5, 200000)
 
     #np.save('tf', tf.toarray())
